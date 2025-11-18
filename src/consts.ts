3c2c069a603818a5ed2b5b6b59e3abc615a4e20d
Ayushman Malla <mall0006@e.ntu.edu.sg>
2025-11-18 17:44:17 +0800

    added internship section and made some more styling changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Ayushman Malla';
+export const SITE_TITLE = 'Home';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
